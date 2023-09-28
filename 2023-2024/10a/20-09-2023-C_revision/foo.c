ae06124dfd7f4577a7c624724b467d3d42132dfb
toniuyt123 <antoniomilev123@gmail.com>
2023-09-28 13:17:06 +0300

    10a first two classes

---
+++ 2023-2024/10a/20-09-2023-C_revision/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "foo.h"
+
+int foo()
+{
+    printf("foo\n");
+}
---
+++ 2023-2024/10a/20-09-2023-C_revision/foo.h
@@ -0,0 +1,8 @@
+
+#ifndef FOO
+#define FOO
+// guard за да се предпазим от това да дефинираме два пъти
+
+int foo();
+
+#endif
